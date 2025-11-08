265ef820838729dc7c68830416fc432981c1728b
Shaik Haleem Pasha <136166687+shaikhaleem11@users.noreply.github.com>
2025-11-08 10:25:01 +0530

    Update SplashActivity.java

--- campus_connect-master/app/src/main/java/com/mcet/campus_connect/SplashActivity.java
+++ campus_connect-master/app/src/main/java/com/mcet/campus_connect/SplashActivity.java
@@ -1,3 +1,25 @@
 package com.mcet.campus_connect;
 
-// Java code for SplashActivity here
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
