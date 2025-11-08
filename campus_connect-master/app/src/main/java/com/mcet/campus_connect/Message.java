7c91b9201e1ae8b07e838b1c9d42285b19d53d72
Shaik Haleem Pasha <136166687+shaikhaleem11@users.noreply.github.com>
2025-11-08 10:22:50 +0530

    Update Message.java

--- campus_connect-master/app/src/main/java/com/mcet/campus_connect/Message.java
+++ campus_connect-master/app/src/main/java/com/mcet/campus_connect/Message.java
@@ -1,3 +1,15 @@
 package com.mcet.campus_connect;
 
-// Java code for Message here
+public class Message {
+    public String message;
+    public String senderId;
+
+    public Message() {
+        // default constructor required for Firebase deserialization
+    }
+
+    public Message(String message, String senderId) {
+        this.message = message;
+        this.senderId = senderId;
+    }
+}
