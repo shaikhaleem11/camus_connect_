9a41ef828f66315ab637d2196ea59cde81054f83
Shaik Haleem Pasha <136166687+shaikhaleem11@users.noreply.github.com>
2025-11-08 10:22:20 +0530

    Update MainActivity.java

--- campus_connect-master/app/src/main/java/com/mcet/campus_connect/MainActivity.java
+++ campus_connect-master/app/src/main/java/com/mcet/campus_connect/MainActivity.java
@@ -1,3 +1,76 @@
 package com.mcet.campus_connect;
 
-// Java code for MainActivity here
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView userRecyclerView;
+    private ArrayList<user> userList;
+    private UserAdatpter adapter;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDbRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDbRef = FirebaseDatabase.getInstance().getReference();
+        userList = new ArrayList<>();
+        adapter = new UserAdatpter(this, userList);
+
+        userRecyclerView = findViewById(R.id.userRecyclerView);
+        userRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userRecyclerView.setAdapter(adapter);
+
+        mDbRef.child("user").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userList.clear();
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    user currentUser = postSnapshot.getValue(user.class);
+                    if (mAuth.getCurrentUser() != null
+                            && currentUser != null
+                            && !mAuth.getCurrentUser().getUid().equals(currentUser.uid)) {
+                        userList.add(currentUser);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // handle error
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            mAuth.signOut();
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            finish();
+            startActivity(intent);
+            return true;
+        }
+        return true;
+    }
+}
