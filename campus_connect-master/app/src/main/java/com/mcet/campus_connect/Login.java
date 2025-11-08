be180dd68c45a1abcc4afa36c7df23a954078f56
Shaik Haleem Pasha <136166687+shaikhaleem11@users.noreply.github.com>
2025-11-08 10:22:00 +0530

    Update Login.java

--- campus_connect-master/app/src/main/java/com/mcet/campus_connect/Login.java
+++ campus_connect-master/app/src/main/java/com/mcet/campus_connect/Login.java
@@ -1,3 +1,59 @@
 package com.mcet.campus_connect;
 
-// Java code for Login here
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnLogin;
+    private Button btnSignUp;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignup);
+
+        btnSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, SignUp.class);
+            startActivity(intent);
+        });
+
+        btnLogin.setOnClickListener(v -> {
+            String email = edtEmail.getText().toString();
+            String password = edtPassword.getText().toString();
+            login(email, password);
+        });
+    }
+
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(Login.this, MainActivity.class);
+                        finish();
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(Login.this, "user does not exist", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
